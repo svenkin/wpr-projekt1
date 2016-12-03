fcd5b9b3c690506ff15c978172383b596b2d214d
Ourea <oureale@gmail.com>
2016-12-03 15:48:14 +0100

    updated some webpack stuff

--- Lernprogramm/WebContent/webpack.config.js
+++ Lernprogramm/WebContent/webpack.config.js
@@ -1,9 +1,28 @@
-const path = require('path');
+const resolve = require('path').resolve;
 
 module.exports = {
     entry: './src/main.js',
     output: {
-        path: path.resolve('dist/'),
+        path: resolve('dist/'),
+        publicPath: '/dist/',
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                exclude: 'node_modules/'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
     }
 };
